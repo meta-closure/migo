87f04fc4759c32b6bd104753c6b54a71fd249dd6
hei <32mens@gmail.com>
2016-08-10 14:49:09 +0900

    add cli command

--- cmd/mig/main.go
+++ cmd/mig/main.go
@@ -7,100 +7,169 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/ghodss/yaml"
-	"github.com/jessevdk/go-flags"
 	"github.com/lestrrat/go-jshschema"
 	"github.com/pkg/errors"
+	"github.com/urfave/cli"
 )
 
-type Option struct {
-	JSON      string `short:"j" long:"json" description:"Source Schema JSON File"`
-	YANL      string `short:"y" long:"yaml" description:"Source Schema YANL File"`
-	StateFile string `short:"i" long:"state file" description:"Internal generated data JSON file"`
+func SetupCmd() *cli.App {
+	app := cli.NewApp()
+
+	app.Name = "mig"
+	app.Usage = "database migrater from a JSON Schema file"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "json, j",
+			Usage: "Load configuration from `Schema` JSON format file.",
+		},
+		cli.StringFlag{
+			Name:  "yaml, y",
+			Usage: "Load configuration from `Schema` YAML format file.",
+		},
+		cli.StringFlag{
+			Name:  "state, s",
+			Usage: "Load internal state from `State` YAML format file.",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "run",
+			Usage:  "run migrate task",
+			Action: Run,
+		},
+		{
+			Name:   "plan",
+			Usage:  "get migration plan from Schema file",
+			Action: Plan,
+		},
+		{
+			Name:   "init",
+			Usage:  "create initial state file",
+			Action: StateInit,
+		},
+	}
+
+	return app
 }
 
-func cmd() (*Option, error) {
-	opts := &Option{}
-	_, err := flags.Parse(opts)
-	if err != nil {
-		return nil, err
-	}
-	return opts, nil
-}
-
-func ParseSchema(h *hschema.HyperSchema, opt *Option) error {
-	if opt.JSON != "" {
-		hs, err := hschema.ReadFile(opt.JSON)
-		if err != nil {
-			return errors.Wrap(err, "JSON file parse error")
-		}
-		h = hs
-		return nil
-	} else if opt.YANL != "" {
-		b, err := ioutil.ReadFile(opt.YANL)
-		if err != nil {
-			return errors.Wrap(err, "YAML file open error")
-		}
-		y := &map[string]interface{}{}
-		err = yaml.Unmarshal(b, y)
-		if err != nil {
-			return errors.Wrap(err, "YAML file parse error")
-		}
-		h.Extract(*y)
-		return nil
-	} else {
-		return errors.New("Source Schema file is empty")
-	}
-}
-
-func _main() int {
-	opt, err := cmd()
-	if err != nil {
-		log.Printf("Command parse error: %v", err)
-		return 1
-	}
+func Parse(c *cli.Context, mode string) error {
 
 	h := hschema.New()
-	err = ParseSchema(h, opt)
+	if j := c.GlobalString("json"); j != "" {
+		err := ParseSchemaJSON(h, j)
+		return errors.Wrap(err, "Parse Schema error")
+
+	} else if y := c.GlobalString("yaml"); y != "" {
+		err := ParseSchemaYAML(h, y)
+		return errors.Wrap(err, "Parse Schema error")
+
+	} else {
+		return errors.New("Source Schema is not specified")
+	}
+
+	s := c.GlobalString("state")
+	if s == "" {
+		return errors.New("State file is not speficied")
+	}
+
+	o, err := mig.ParseState(s)
 	if err != nil {
-		log.Printf("HyperSchema parse error: %v", err)
-		return 1
+		return errors.Wrap(err, "State YAML file parse error")
 	}
 
 	n, err := mig.ParseSchema2State(h)
 	if err != nil {
-		log.Printf("Parse HyperSchema to State failed: %v", err)
-		return 1
-	}
+		return errors.Wrap(err, "Parse HyperSchema to State failed")
 
-	o, err := mig.ParseState(opt.StateFile)
-	if err != nil {
-		log.Printf("State YAML file parse error: %v", err)
-		return 1
 	}
 
 	sql, err := o.SQLBuilder(n)
 	if err != nil {
-		log.Printf("SQL Build error: %v", err)
-		return 1
+		return errors.Wrap(err, "SQL Build error")
 	}
 
 	sql.Check()
 
+	if mode == "plan" {
+		return nil
+	}
+
 	err = sql.Migrate()
 	if err != nil {
-		log.Printf("Database migration error: %v", err)
-		return 1
+		return errors.Wrap(err, "Database migration error")
 	}
 
-	err = n.Update(opt.StateFile)
+	return nil
+
+	err = n.Update(s)
 	if err != nil {
-		log.Printf("Failed to save State file: %v", err)
-		return 1
+		return errors.Wrap(err, "Failed to save State file")
 	}
 
+	return nil
+}
+
+func Run(c *cli.Context) error {
+	err := Parse(c, "run")
+	if err != nil {
+		return errors.Wrap(err, "RUN")
+	}
+	return nil
+}
+
+func Plan(c *cli.Context) error {
+	err := Parse(c, "plan")
+	if err != nil {
+		return errors.Wrap(err, "PLAN")
+	}
+	return nil
+}
+
+func StateInit(c *cli.Context) error {
+	n := mig.StateNew()
+	err := n.Update("./database_state.yml")
+	if err != nil {
+		return errors.Wrap(err, "StateInit")
+	}
+	return nil
+}
+
+func _cmd() int {
+	cmd := SetupCmd()
+	err := cmd.Run(os.Args)
+	if err != nil {
+		log.Error(err)
+		return 1
+	}
 	return 0
 }
 
+func ParseSchemaYAML(h *hschema.HyperSchema, s string) error {
+	b, err := ioutil.ReadFile(s)
+	if err != nil {
+		return errors.Wrap(err, "YAML file open error")
+	}
+	y := &map[string]interface{}{}
+	err = yaml.Unmarshal(b, y)
+	if err != nil {
+		return errors.Wrap(err, "YAML file parse error")
+	}
+	h.Extract(*y)
+
+	return nil
+}
+
+func ParseSchemaJSON(h *hschema.HyperSchema, s string) error {
+	hs, err := hschema.ReadFile(s)
+	if err != nil {
+		return errors.Wrap(err, "JSON file parse error")
+	}
+	h = hs
+	return nil
+}
+
 func main() {
-	os.Exit(_main())
+	os.Exit(_cmd())
 }
