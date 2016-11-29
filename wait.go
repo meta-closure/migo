08692c1724fc3b1ad0568a4a66b829732784e9bb
minodisk <daisuke.mino@gmail.com>
2016-11-29 12:45:10 +0900

    Add timeout for waiting
    Fixes #15

--- wait.go
+++ wait.go
@@ -9,6 +9,10 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	timeoutForWait = time.Minute
+)
+
 type Logger struct{}
 
 func (l Logger) Print(v ...interface{}) {
@@ -35,9 +39,14 @@ func DbWait(dbpath, env string) error {
 	}
 
 	fmt.Printf("Waiting for accepting query ")
+	startedAt := time.Now()
 	for {
 		fmt.Printf(".")
 		query := fmt.Sprintf("show databases")
+		if time.Now().Sub(startedAt) > timeoutForWait {
+			fmt.Printf("\n")
+			return errors.New("Timeout for waiting")
+		}
 		if _, err := db.Exec(query); err == nil {
 			fmt.Printf("\nAccepted\n")
 			break
