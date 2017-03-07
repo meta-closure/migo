7dd8be58ded362becb29f98f26a88b781460bc72
meta-closure <32mens@gmail.com>
2017-03-07 15:55:53 +0900

    update dependency

--- vendor/github.com/lestrrat/go-jsref/README.md
+++ vendor/github.com/lestrrat/go-jsref/README.md
@@ -9,11 +9,14 @@ JSON Reference Implementation for Go
 # SYNOPSIS
 
 ```go
+package jsref_test
+
 import (
   "encoding/json"
+  "fmt"
   "log"
 
-  "github.com/lestrrat/go-jsref"
+  jsref "github.com/lestrrat/go-jsref"
   "github.com/lestrrat/go-jsref/provider"
 )
 
@@ -21,7 +24,7 @@ func Example() {
   var v interface{}
   src := []byte(`
 {
-  "foo": ["bar", {"$ref": "#/sub"}, {"$ref", "obj2#/sub"}],
+  "foo": ["bar", {"$ref": "#/sub"}, {"$ref": "obj2#/sub"}],
   "sub": "baz"
 }`)
   if err := json.Unmarshal(src, &v); err != nil {
@@ -36,9 +39,44 @@ func Example() {
   res := jsref.New()
   res.AddProvider(mp) // Register the provider
 
-  res.Resolve(v, "#/foo/0") // "bar"
-  res.Resolve(v, "#/foo/1") // "baz"
-  res.Resolve(v, "#/foo/2") // "quux" (resolve via `mp`)
+  data := []struct {
+    Ptr string
+    Options []jsref.Option
+  }{
+    {
+      Ptr: "#/foo/0", // "bar"
+    },
+    {
+      Ptr: "#/foo/1", // "baz"
+    },
+    {
+      Ptr: "#/foo/2", // "quux" (resolves via `mp`)
+    },
+    {
+      Ptr: "#/foo",   // ["bar",{"$ref":"#/sub"},{"$ref":"obj2#/sub"}]
+    },
+    {
+      Ptr: "#/foo",   // ["bar","baz","quux"]
+      // experimental option to resolve all resulting values
+      Options: []jsref.Option{ jsref.WithRecursiveResolution(true) },
+    },
+  }
+  for _, set := range data {
+    result, err := res.Resolve(v, set.Ptr, set.Options...)
+    if err != nil { // failed to resolve
+      fmt.Printf("err: %s\n", err)
+      continue
+    }
+    b, _ := json.Marshal(result)
+    fmt.Printf("%s -> %s\n", set.Ptr, string(b))
+  }
+
+  // OUTPUT:
+  // #/foo/0 -> "bar"
+  // #/foo/1 -> "baz"
+  // #/foo/2 -> "quux"
+  // #/foo -> ["bar",{"$ref":"#/sub"},{"$ref":"obj2#/sub"}]
+  // #/foo -> ["bar","baz","quux"]
 }
 ```
 
@@ -60,6 +98,10 @@ Currently available `Provider`s are:
 | Name                                                     | Notes                            |
 |:--------------------------------------------------------:|:---------------------------------|
 | [go-jsval](https://github.com/lestrrat/go-jsval)         | Validator generator              |
-| [go-jshschema](https://github.com/lestrrat/go-jsschema)  | JSON Hyper Schema implementation |
+| [go-jshschema](https://github.com/lestrrat/go-jshschema)  | JSON Hyper Schema implementation |
 | [go-jsschema](https://github.com/lestrrat/go-jsschema)   | JSON Schema implementation       |
 | [go-jspointer](https://github.com/lestrrat/go-jspointer) | JSON Pointer implementations     |
+
+# Acknowledgements
+
+* Boris Burtin
--- vendor/github.com/lestrrat/go-jsref/jsref_test.go
+++ vendor/github.com/lestrrat/go-jsref/jsref_test.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -17,30 +18,6 @@ import (
 	"github.com/lestrrat/go-jsref/provider"
 	"github.com/stretchr/testify/assert"
 )
-
-func Example() {
-	var v interface{}
-	src := []byte(`
-{
-  "foo": ["bar", {"$ref": "#/sub"}, {"$ref", "obj2#/sub"}],
-  "sub": "baz"
-}`)
-	if err := json.Unmarshal(src, &v); err != nil {
-		log.Printf("%s", err)
-		return
-	}
-
-	// External reference
-	mp := provider.NewMap()
-	mp.Set("obj2", map[string]string{"sub": "quux"})
-
-	res := jsref.New()
-	res.AddProvider(mp) // Register the provider
-
-	res.Resolve(v, "#/foo/0") // "bar"
-	res.Resolve(v, "#/foo/1") // "baz"
-	res.Resolve(v, "#/foo/2") // "quux" (resolve via `mp`)
-}
 
 func TestResolveMemory(t *testing.T) {
 	m := map[string]interface{}{
@@ -82,6 +59,17 @@ func TestResolveMemory(t *testing.T) {
 		if !assert.Equal(t, v, expected, "Resolve(%s) resolves to '%s'", ptr, expected) {
 			return
 		}
+	}
+
+	// In this test we test if we can optionally recursively
+	// resolve references
+	v, err := res.Resolve(m, "#/foo", jsref.WithRecursiveResolution(true))
+	if !assert.NoError(t, err, "Resolve(%s) should succeed", "#/foo") {
+		return
+	}
+
+	if !assert.Equal(t, []interface{}{"bar", "baz", "quux"}, v) {
+		return
 	}
 }
 
@@ -191,7 +179,7 @@ func TestResolveRecursive(t *testing.T) {
 {
 	"foo": {
 		"type": "array",
-		"items": { "$ref": "#" }
+		"items": [{ "$ref": "#" }]
 	}
 }`)
 	if err := json.Unmarshal(src, &v); err != nil {
@@ -204,4 +192,104 @@ func TestResolveRecursive(t *testing.T) {
 	if !assert.NoError(t, err, "res.Resolve should succeed") {
 		return
 	}
+}
+
+func TestGHPR12(t *testing.T) {
+	// https://github.com/lestrrat/go-jsref/pull/2 gave me an example
+	// using "foo" as the JS pointer (could've been a typo)
+	// but it gave me weird results, so this is where I'm testing it
+	var v interface{}
+	src := []byte(`
+{
+	"foo": "bar"
+}`)
+	if err := json.Unmarshal(src, &v); err != nil {
+		log.Printf("%s", err)
+		return
+	}
+
+	res := jsref.New()
+	_, err := res.Resolve(v, "foo")
+	if !assert.NoError(t, err, "res.Resolve should fail") {
+		return
+	}
+}
+
+func TestHyperSchemaRecursive(t *testing.T) {
+	src := []byte(`
+{
+  "definitions": {
+    "virtual_machine": {
+      "type": "object"
+    }
+  },
+  "links": [
+    {
+      "schema": {
+        "type": "object"
+      },
+      "targetSchema": {
+        "$ref": "#/definitions/virtual_machine"
+      }
+    },
+    {
+      "targetSchema": {
+        "type": "array",
+        "items": {
+          "$ref": "#/definitions/virtual_machine"
+        }
+      }
+    }
+  ]
+}`)
+	var v interface{}
+	err := json.Unmarshal(src, &v)
+	assert.Nil(t, err)
+	res := jsref.New()
+
+	ptrs := []string{
+		"#/links/0/schema",
+		"#/links/0/targetSchema",
+		"#/links/1/targetSchema",
+	}
+	for _, ptr := range ptrs {
+		result, err := res.Resolve(v, ptr, jsref.WithRecursiveResolution(true))
+		assert.Nil(t, err)
+		b, err := json.Marshal(result)
+		if !assert.NoError(t, err, "json.Marshal should succeed") {
+			return
+		}
+		if !assert.False(t, strings.Contains(string(b), "$ref"), "%s did not recursively resolve", ptr) {
+			t.Logf("resolved to '%s'", b)
+			return
+		}
+	}
+}
+
+func TestGHIssue7(t *testing.T) {
+	src := []byte(`{
+  "status": {
+    "type": ["string", "null"],
+    "enum": [
+      "sent",
+      "duplicate",
+      "error",
+      "invalid",
+      "rejected",
+      "unqueued",
+      "unsubscribed",
+      null
+    ]
+  }
+}`)
+
+	var v interface{}
+	if !assert.NoError(t, json.Unmarshal(src, &v), `Unmarshal should succeed`) {
+		return
+	}
+
+	res := jsref.New()
+	result, err := res.Resolve(v, "", jsref.WithRecursiveResolution(true))
+	t.Logf("%s", result)
+	t.Logf("%s", err)
 }
